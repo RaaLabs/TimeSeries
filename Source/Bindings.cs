387e6f738a3709e916bc1611644c99a6c6a99938
Einar Ingebrigtsen <einar@dolittle.com>
2019-03-21 09:33:26 +0100

    Moved all code from IdentityMapper

---
+++ Source/Bindings.cs
@@ -0,0 +1,26 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Dolittle. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+using System;
+using Dolittle.DependencyInversion;
+using Microsoft.Azure.Devices.Client;
+using Microsoft.Azure.Devices.Client.Transport.Mqtt;
+
+namespace Dolittle.Edge.Modules
+{
+    /// <summary>
+    /// Provides bindings
+    /// </summary>
+    public class Bindings : ICanProvideBindings
+    {
+        /// <inheritdoc/>
+        public void Provide(IBindingProviderBuilder builder)
+        {
+            if (string.IsNullOrEmpty(Environment.GetEnvironmentVariable("IOTEDGE_WORKLOADURI")))
+                builder.Bind<IClient>().To<NullClient>();
+            else
+                builder.Bind<IClient>().To<Client>();
+        }
+    }
+}
---
+++ Source/NullClient.cs
@@ -0,0 +1,27 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Dolittle. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+using System.Threading.Tasks;
+using Microsoft.Azure.Devices.Client;
+
+namespace Dolittle.Edge.Modules
+{
+    /// <summary>
+    /// Represents a null implementation of <see cref="IClient"/>
+    /// </summary>
+    public class NullClient : IClient
+    {
+        /// <inheritdoc/>
+        public Task SendEvent(Output output, Message message)
+        {
+            return Task.CompletedTask;
+        }
+
+        /// <inheritdoc/>
+        public Task SetInputMessageHandler(Input input, MessageHandler messageHandler, object userContext)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
