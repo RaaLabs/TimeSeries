a8eaa01f7a77b7856a765bb6f8c40c8dad42d1bb
sigbjorn <sigbjorn.rudaa@gmail.com>
2020-01-14 12:05:36 +0100

    Fix desired properties, previous version with getContainer is better.

--- Source/TimeSeries.Modules.IoTEdge/DesiredPropertiesConfigurationObjectProvider.cs
+++ Source/TimeSeries.Modules.IoTEdge/DesiredPropertiesConfigurationObjectProvider.cs
@@ -5,6 +5,7 @@
 using System;
 using Dolittle.Configuration;
 using Dolittle.Configuration.Files;
+using Dolittle.DependencyInversion;
 using Dolittle.Logging;
 using Dolittle.Strings;
 using Microsoft.Azure.Devices.Client;
@@ -18,7 +19,7 @@ namespace RaaLabs.TimeSeries.Modules.IoTEdge
     /// </summary>
     public class DesiredPropertiesConfigurationObjectProvider : ICanProvideConfigurationObjects
     {
-        readonly ModuleClient _client;
+        readonly GetContainer _getContainer;
         readonly IConfigurationFileParsers _parsers;
         readonly ILogger _logger;
         Twin _twin;
@@ -26,33 +27,33 @@ namespace RaaLabs.TimeSeries.Modules.IoTEdge
         /// <summary>
         /// Initializes a new instance of <see cref="DesiredPropertiesConfigurationObjectProvider"/>
         /// </summary>
-        /// <param name="client">Underlying <see cref="ModuleClient"/></param>
+        /// <param name="getContainer"><see cref="GetContainer"/> for getting access to the IoC container</param>
         /// <param name="parsers"><see cref="IConfigurationFileParsers"/> for parsing configuration</param>
         /// <param name="logger"><see cref="ILogger"/> for logging</param>
         public DesiredPropertiesConfigurationObjectProvider(
-            ModuleClient client,
+            GetContainer getContainer,
             IConfigurationFileParsers parsers,
             ILogger logger)
         {
-            _client = client;
-
-            if (IoTEdgeHelpers.IsRunningInIotEdge())
-            {
-                client.GetTwinAsync()
-                    .ContinueWith(_ => _twin = _.Result)
-                    .Wait();
-            }
-
             _parsers = parsers;
             _logger = logger;
-
-            _logger.Information($"Desired properties : {_twin.Properties.Desired.ToJson()}");
+            _getContainer = getContainer;
         }
 
         /// <inheritdoc/>
         public bool CanProvide(Type type)
         {
-            if (IoTEdgeHelpers.IsRunningInIotEdge()) return false;
+            if (!IoTEdgeHelpers.IsRunningInIotEdge()) return false;
+
+            if( _twin == null )
+            {               
+                var client = _getContainer().Get<ModuleClient>();
+                client.GetTwinAsync()
+                    .ContinueWith(_ => _twin = _.Result)
+                    .Wait();
+
+                _logger.Information($"Desired properties : {_twin.Properties.Desired.ToJson()}");
+            }
 
             var name = type.GetFriendlyConfigurationName().ToCamelCase();
             _logger.Information($"Ask for providing {name}");
